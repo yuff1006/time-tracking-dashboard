9c07c541bdf9a6b9d05a1a617478c5e12ca7fe97
Yuffie Hu <yuffieevans@gmail.com>
2022-04-27 23:17:54 -0500

    restore

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,9 @@
-import { Card } from "./Tracker.js";
-const dailyCardDeck = [];
-const weeklyCardDeck = [];
-const monthlyCardDeck = [];
-fetch("data.json")
-  .then((res) => res.json())
-  .then((data) => {
-    const trackerData = data;
-    trackerData.forEach((obj) => {
-      const dailyCard = new Card(obj.title, "daily");
-      dailyCardDeck.push(dailyCard);
-      const monthlyCard = new Card(obj.title, "monthly");
-      monthlyCardDeck.push(monthlyCard);
-      const weeklyCard = new Card(obj.title, "weekly");
-      weeklyCardDeck.push(weeklyCard);
-    });
-    console.log(dailyCardDeck);
-  });
-
-// import { Card, trackerData } from "./Tracker.js";
+import { Card, trackerData } from "./Tracker.js";
 
 const dailyButton = document.querySelector("#daily");
 const weeklyButton = document.querySelector("#weekly");
 const monthlyButton = document.querySelector("#monthly");
 const cardsContainer = document.querySelector(".tracker");
-
 function resetCards() {
   while (cardsContainer.lastChild.id !== "profile") {
     cardsContainer.removeChild(cardsContainer.lastChild);
@@ -38,36 +18,34 @@ dailyButton.addEventListener("click", () => {
   resetCards();
   resetButtons();
   dailyButton.classList.add("selected");
-  const cardEl = dailyCardDeck.forEach((cardEl) => {
-    cardEl.generateCard();
+  trackerData.forEach((obj) => {
+    const dailyCard = new Card(obj.title, "daily");
+    cardsContainer.append(dailyCard.generateCard());
   });
-  cardsContainer.append(cardEl);
 });
 weeklyButton.addEventListener("click", () => {
   resetCards();
   resetButtons();
   weeklyButton.classList.add("selected");
-  const cardEl = weeklyCardDeck.forEach((cardEl) => {
-    cardEl.generateCard();
+  trackerData.forEach((obj) => {
+    const weeklyCard = new Card(obj.title, "weekly");
+    cardsContainer.append(weeklyCard.generateCard());
   });
-  cardsContainer.append(cardEl);
 });
 monthlyButton.addEventListener("click", () => {
   resetCards();
   resetButtons();
   monthlyButton.classList.add("selected");
-  const cardEl = monthlyCardDeck.forEach((cardEl) => {
-    cardEl.generateCard();
+  trackerData.forEach((obj) => {
+    const monthlyCard = new Card(obj.title, "monthly");
+    cardsContainer.append(monthlyCard.generateCard());
   });
-  cardsContainer.append(cardEl);
 });
 
 window.addEventListener("load", () => {
-  weeklyButton.classList.add("selected");
-  const cardEl = weeklyCardDeck.forEach((cardEl) => {
-    cardEl.generateCard();
+  trackerData.forEach((obj) => {
+    const weeklyCard = new Card(obj.title, "weekly");
+    cardsContainer.append(weeklyCard.generateCard());
   });
-  cardsContainer.append(cardEl);
+  weeklyButton.classList.add("selected");
 });
-
-console.log(dailyCardDeck);
